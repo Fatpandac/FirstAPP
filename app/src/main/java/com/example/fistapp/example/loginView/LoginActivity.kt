c42a964cd21a9b1936ede76e53af784ceaf2b1b6
Fatpandac <i@fatpandac.com>
2022-12-01 09:04:59 +0800

    feat(view): add remember password

--- app/src/main/java/com/example/fistapp/example/loginView/LoginActivity.kt
+++ app/src/main/java/com/example/fistapp/example/loginView/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.example.fistapp.R
@@ -13,12 +14,31 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        val accountInput = findViewById<EditText>(R.id.account)
+        val passwordInput = findViewById<EditText>(R.id.password)
+        val rememberCheckBox = findViewById<CheckBox>(R.id.rememberCheckBox)
         val btn = findViewById<Button>(R.id.login)
+        val sharedPreferences = getSharedPreferences("account", MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+
+        val account = sharedPreferences.getString("account", "")
+        val password = sharedPreferences.getString("password", "")
+        accountInput.setText(account)
+        passwordInput.setText(password)
+
+
+
         btn.setOnClickListener {
-            val accountInput = findViewById<EditText>(R.id.account)
-            val passwordInput = findViewById<EditText>(R.id.password)
             Log.e("Login:", "${passwordInput.text} ${accountInput.text}")
             if (accountInput.text.toString() == "i@fatpandac.com" && passwordInput.text.toString() == "123456") {
+                if (rememberCheckBox.isChecked) {
+                    editor.putString("account", accountInput.text.toString())
+                    editor.putString("password", passwordInput.text.toString())
+                } else {
+                    editor.putString("account", "")
+                    editor.putString("password", "")
+                }
+                editor.apply()
                 val newsIntent = Intent(this, HelloActivity::class.java)
                 startActivity(newsIntent)
             }
